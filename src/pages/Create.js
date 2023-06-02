039b30a355fd936d5a44bd7b9cf31e76181b5fb3
Aditya Raul <raul.aditya912@gmail.com>
2023-06-02 12:09:57 +0530

    button icon complete

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,3 +1,26 @@
+import { Typography, Button, Container } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
 export const Create = () => {
-  return <div>Create</div>;
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        component="h2"
+        color="text.secondary"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+      <Button
+        type="submit"
+        color="secondary"
+        variant="contained"
+        onClick={() => console.log("Clicked")}
+        endIcon={<SendIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 };
